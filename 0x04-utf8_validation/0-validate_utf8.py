2a14cf450c0e4c41b673eb7e33e0bcd4c16b01bc
Zeinu Kedir <zeinuka641@gmail.com>
2024-09-03 06:08:23 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,44 +4,58 @@
 
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
-
-    # Masks to check the most significant bits (MSBs)
-    mask1 = 1 << 7  # 10000000 in binary
-    mask2 = 1 << 6  # 01000000 in binary
-
-    # Iterate over each byte in the data
-    for byte in data:
-        # Get the 8 least significant bits of the byte
-        byte = byte & 0xFF
-
-        if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte & mask1) == 0:
-                # 1-byte character (ASCII)
-                continue
-            elif (byte & (mask1 >> 1)) == mask1:
-                # Invalid (leading bits are '11',
-                # but expecting 2, 3, or 4 bytes)
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
                 return False
-            elif (byte & (mask1 >> 2)) == mask1 >> 1:
-                # 2-byte character
-                num_bytes = 1
-            elif (byte & (mask1 >> 3)) == mask1 >> 2:
-                # 3-byte character
-                num_bytes = 2
-            elif (byte & (mask1 >> 4)) == mask1 >> 3:
-                # 4-byte character
-                num_bytes = 3
+        elif data[i] & 0b11110000 == 0b11100000:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
             else:
                 return False
         else:
-            # Continuation bytes must start with '10xxxxxx'
-            if (byte & mask1) == mask1 and (byte & mask2) == 0:
-                num_bytes -= 1
-            else:
-                return False
-
-    # If num_bytes is not 0, then we were expecting more continuation bytes
-    return num_bytes == 0
+            return False
+    return True
